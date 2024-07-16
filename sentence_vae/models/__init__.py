49c43ce59afb9c89f0e29b8631657431790dfc2b
Coder.AN <an.hongjun@foxmail.com>
2024-07-16 02:35:53 +0000

    del old version

--- sentence_vae/models/__init__.py
+++ sentence_vae/models/__init__.py
@@ -30,6 +30,5 @@ from .positional_encoding import PositionalEncodding
 from .sentence_encoder import SentenceEncoder
 from .sentence_decoder import SentenceDecoder
 from .sentence_vae_model import SentenceVAE
-from .llm_vae_model import LLMSentenceVAE
 from .sentence_llm_model import SentenceLLM
 from .focal_loss import FocalLoss
