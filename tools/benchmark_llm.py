0e1030c5a27dabc87bc2c5dde55883180db7aff5
Coder.AN <an.hongjun@foxmail.com>
2024-07-21 03:21:24 +0000

    add llm benchmark

--- tools/benchmark_llm.py
+++ tools/benchmark_llm.py
@@ -1,22 +1,40 @@
-import os
-import re
-import sys 
-import json
+# Copyright (c) 2024, School of Artificial Intelligence, OPtics and 
+# ElectroNics(iOPEN), Northwestern PolyTechnical University, and Institute of 
+# Artificial Intelligence (TeleAI), China Telecom.
+#
+# Author:   Coder.AN (an.hongjun@foxmail.com)
+#           Huasen Chen (chenyifan1@mail.nwpu.edu.cn)
+# 
+# 
+# This software is licensed under the MIT License.
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 import time
-import shutil
 import argparse
 import numpy as np
 from tqdm import tqdm
 
 import torch
-import torch.nn.functional as F
 from torch.utils.data import DataLoader
-from torch.utils.tensorboard import SummaryWriter
 
-from mmengine.config import Config
-from mmengine.runner import Runner, IterBasedTrainLoop 
 from mmengine.dataset import DefaultSampler
-from mmengine.dist.utils import init_dist
 
 from sentence_vae.utils import get_model, get_tokenizer, get_dtype
 from sentence_vae.models import SentenceVAE
